ca9d069472862084e1cb6b8beb9e7f859238b3e4
Vera Lavrentyeva <55912590+TwilightHunt@users.noreply.github.com>
2023-08-10 13:54:01 +0300

    build: display progress

--- src/components/player.vue
+++ src/components/player.vue
@@ -9,6 +9,7 @@ defineProps<PlayerProps>();
 
 const video: Ref<HTMLVideoElement | undefined> = ref();
 const isVideoPlayed: Ref<boolean> = ref(false);
+const progress: Ref<HTMLProgressElement | undefined> = ref();
 
 const play = () => {
   video.value?.play();
@@ -19,11 +20,28 @@ const pause = () => {
   video.value?.pause();
   isVideoPlayed.value = false;
 };
+
+const displayProgress = () => {
+  const { duration, currentTime } = video.value as HTMLVideoElement;
+  if (progress.value !== undefined) {
+    progress.value.value = (100 * currentTime) / duration;
+  }
+};
+const rewind = (event: MouseEvent) => {
+  const target = event.target as HTMLProgressElement;
+  const width = target.offsetWidth;
+  const offsetX = event.offsetX;
+  target.value = (100 * offsetX) / width;
+  pause();
+  if (video.value) {
+    video.value.currentTime = video.value.duration * (offsetX / width);
+  }
+};
 </script>
 
 <template>
   <div class="player">
-    <video ref="video" class="player__video">
+    <video @timeupdate="displayProgress" ref="video" class="player__video">
       <source :src="src" />
     </video>
 
@@ -33,6 +51,13 @@ const pause = () => {
     <div v-else class="player__icon" @click="play">
       <icon-play id="play" />
     </div>
+    <progress
+      class="cursor-pointer player__progress"
+      ref="progress"
+      max="100"
+      value="0"
+      @mousedown="rewind"
+    />
   </div>
 </template>
 
@@ -57,6 +82,15 @@ const pause = () => {
         transform: scale(1.3);
       }
     }
+  }
+  &__progress {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 35px;
+    padding: 1rem;
+
+    width: 100%;
   }
 }
 </style>
